e0d78e5dfcde55966d3c84ddb9843df51751677f
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-07-29 12:00:24 +0900

    add softmax class

--- src/softmax.py
+++ src/softmax.py
@@ -3,6 +3,7 @@
 from greedy import Greedy
 from numpy.random import rand
 import random
+import numpy as np
 
 
 class Softmax(Greedy):
@@ -22,6 +23,36 @@ class Softmax(Greedy):
     def select_arm(self):
         """Select arm with Softmax algorthm."""
         trials = [self.trials[i] for i in range(self.K)]
+
+        if 0 in trials:  # 最初全てのアームを一回ずつ選択する
+            return trials.index(0)
+        else:
+            mus = [self.hits[i] / trials[i] for i in range(self.K)]
+            scores = self._softmax(mus)
+            return self._sampling_from_pdf(scores)
+
+    def _softmax(self, mus):
+        """Softmax function.
+
+        :param mus: list of mu(float)
+        :return scores: list of float
+        """
+        denom = sum([np.exp(mus[i] / self.temp) for i in range(self.K)])
+        return [np.exp(mus[i] / self.temp) / denom for i in range(self.K)]
+
+    def _sampling_from_pdf(self, probabilities):
+        """Sample from probabilistic distribution function.
+
+        :param probabilities:list of probability(sum of them equals to 1)
+        :return arm_num:sampled arm_num from pdf
+        """
+        probabilities = [sum(probabilities[:x + 1])
+                         for x in range(len(probabilities))]
+        rand = random.random()
+        for arm_num, probability in enumerate(probabilities):
+            if rand < probability:
+                return arm_num
+        return None
 
     def __str__(self):
         """Override str method."""
