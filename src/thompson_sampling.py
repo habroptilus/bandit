e6664e61fb1b2b9cdc6c8ca269d7e3d4b4b9fad9
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-07-29 13:18:37 +0900

    add thompson sampling class

---
+++ src/thompson_sampling.py
@@ -0,0 +1,24 @@
+"""Thompson Sampling algorithm."""
+
+from greedy import Greedy
+from numpy.random import rand, beta
+import random
+
+
+class Thompson_sampling(Greedy):
+    """Thompson Sampling algorithm."""
+
+    def select_arm(self):
+        """Select arm with Thompson Sampling algorthm."""
+        beta_randoms = [beta(self.hits[i] + 1, self.trials[i] -
+                             self.hits[i] + 1) for i in range(self.K)]
+        m = max(beta_randoms)
+        return beta_randoms.index(m)
+
+    def __str__(self):
+        """Override str method."""
+        return "<Thompson Sampling> N={} Arms={}".format(self.N, self.arms)
+
+    def __repr__(self):
+        """Override repr method."""
+        return "Thompson_sampling({},{})".format(self.arms, self.N)
